403773fa1843a9a2e01332f33b598ef23ae19d36
origin <sskkccha@gmail.com>
2022-09-27 23:49:50 +0900

    Initial commit

---
+++ app/src/main/java/com/chahye/datatransferjava/FirstFragment.java
@@ -0,0 +1,35 @@
+package com.chahye.datatransferjava;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.chahye.datatransferjava.databinding.FragmentFirstBinding;
+
+public class FirstFragment extends Fragment {
+    private FragmentFirstBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.button.setOnClickListener(v -> {
+            Navigation.findNavController(view).navigate(R.id.action_firstFragment_to_secondFragment);
+        });
+    }
+
+}
